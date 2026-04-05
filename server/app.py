4bf5f05c25efcc1c8ab1251d6a8eb5eaa5c5a166
anuragshrikhandkar <167197574+anuragshrikhandkar@users.noreply.github.com>
2026-04-05 21:14:15 +0530

    update

--- server/app.py
+++ server/app.py
@@ -1,5 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
 def main():
     import uvicorn
-    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+    uvicorn.run("app:app", host="0.0.0.0", port=7860)
